302895e1cf8d3ae3a677532b313454503ec2caf9
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2020-05-31 02:42:43 +0200

    added counting and more filtering options

--- main.js
+++ main.js
@@ -4,10 +4,43 @@ $(document).ready(function () {
     run();
 });
 
+let list = ["farming", "mining", "woodcutting", "fishing"]
 
-let data;
+let data, sorted;
 
 async function run() {
+    setTimeout(async () => {
+        data = await axios.get('https://cors-anywhere.herokuapp.com/https://wynnpendium.ehtycscythe.com/api/node_list.php?prof=all');
+        data = data.data;
+        sorted = [[], [], [], []];
+        for (const x in data) {
+            let i = list.indexOf(x);
+            if (i !== -1) {
+                let current = data[x];
+                for (const y in current) {
+                    let object = current[y];
+                    object.name = y;
+                    sorted[i].push(object);
+                }
+                sorted[i] = sorted[i].sort((a, b) => {
+                    return b.level - a.level;
+                });
+            }
+        }
+        let elements = document.getElementsByName('ts');
+        elements.forEach((element) => {
+            let o = document.createElement("option");
+            o.text = "All";
+            element.add(o);
+            element.selectedIndex = 0;
+            let i = list.indexOf(element.id.toLowerCase());
+            for (const x of sorted[i]) {
+                let option = document.createElement("option");
+                option.text = `${x.name} (Level ${x.level})`;
+                element.add(option);
+            }
+        });
+    }, 1);
     let bounds = [];
     let images = [];
 
@@ -49,44 +82,42 @@ async function run() {
     map.on('zoomend', () => {
         prevZoom = map.getZoom();
     });
-    data = await axios.get('https://cors-anywhere.herokuapp.com/https://wynnpendium.ehtycscythe.com/api/node_list.php?prof=all');
-    data = data.data;
-    console.log(data);
-
 }
 
-let markers = [[],[],[],[]];
+let markers = [[], [], [], []];
 
-async function reload(changed)
-{
+async function reload(changed) {
+    console.log("reloading!")
     setTimeout(() => {
         let elements = document.getElementsByName('cm');
-        for (let i = 0; i < 4; i++) {
-            if (!elements[i].checked) 
-            {
-                if (i === changed)
-                {
-                    markers[i].forEach(element => {
+        let selectors = document.getElementsByName('ts');
+        let counts = document.getElementsByName('pc');
+        [0,1,2,3].forEach(async (ic) => {
+            let i = ic;
+                if (i === changed) {
+                    markers[i].forEach(async (element) => {
                         map.removeLayer(element);
                     });
                     markers[i] = [];
                 }
-                continue;
-            }
-            for (const x in data[elements[i].id.toLowerCase()]) {
-                //console.log(data[elements[i].id.toLowerCase()][x]);
-                setTimeout(() => {
-                    for (const y of data[elements[i].id.toLowerCase()][x].locations) {
+                if (!elements[i].checked) return;
+                let count = 0;
+                for (const x in data[elements[i].id.toLowerCase()]) {
+                    if (selectors[i].selectedIndex === 0 || selectors[i].options[selectors[i].selectedIndex].text.toLowerCase().startsWith(x.toLowerCase())) {
                         setTimeout(() => {
-                            let marker = L.marker([y.z*0.001*-1, y.x*0.001]).addTo(map);
-                            marker.bindPopup(`<b>${x}</b><br>LVL ${data[elements[i].id.toLowerCase()][x].level}<br>X: ${y.x}<br>Y: ${y.y}<br>Z: ${y.z}`);
-                            markers[i].push(marker);
+                            for (const y of data[elements[i].id.toLowerCase()][x].locations) {
+                                setTimeout(() => {
+                                    let marker = L.marker([y.z * 0.001 * -1, y.x * 0.001]).addTo(map);
+                                    marker.bindPopup(`<b>${x}</b><br>LVL ${data[elements[i].id.toLowerCase()][x].level}<br>X: ${y.x}<br>Y: ${y.y}<br>Z: ${y.z}`);
+                                    markers[i].push(marker);
+                                }, 1);
+                                ++count;
+                                counts[i].innerText = `Count: ${count}`;
+                                //console.log(y);
+                            }
                         }, 1);
-                        //console.log(y);
                     }
-                }, 1);
-            }
-            
-        }
+                }
+        });
     }, 1);
 }
